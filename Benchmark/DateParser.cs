29eb6cc3fa5f85a239517ad6afcd337d44e74801
mv89 <markoczvvujic@hotmail.com>
2023-07-17 21:34:23 +0200

    first commit

---
+++ Benchmark/DateParser.cs
@@ -0,0 +1,33 @@
+﻿namespace Benchmark
+{
+    public class DateParser
+    {
+        public int GetYearFromDateTime(string dateTimeAsString)
+        {
+            var dateTime = DateTime.Parse(dateTimeAsString);
+
+            return dateTime.Year;
+        }
+
+        public int GetYearFromSplit(string dateTimeAsString)
+        {
+            var splitOnHyphen = dateTimeAsString.Split('-');
+
+            return int.Parse(splitOnHyphen[0]);
+        }
+
+        public int GetYearFromSubstring(string dateTimeAsString)
+        {
+            var indexofHyphen = dateTimeAsString.IndexOf("-");
+
+            return int.Parse(dateTimeAsString.Substring(0, indexofHyphen));
+        }
+
+        public int GetYearFromSpan(ReadOnlySpan<char> dateTimeAsSpan)
+        {
+            var indexOfHyphen = dateTimeAsSpan.IndexOf("-");
+
+            return int.Parse(dateTimeAsSpan.Slice(0, indexOfHyphen));
+        }
+    }
+}
---
+++ Benchmark/DateParserBenchmark.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class DateParserBenchmark
+    {
+        private const string dateTime = "2023-07-17 15:28:45";
+
+        private  readonly DateParser _parser;
+
+        public DateParserBenchmark()
+        {
+            _parser = new DateParser();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void GetYerFromDateTime()
+        {
+            _parser.GetYearFromDateTime(dateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSplit()
+        {
+            _parser.GetYearFromSplit(dateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSubstring()
+        {
+            _parser.GetYearFromSubstring(dateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSpan()
+        {
+            _parser.GetYearFromSpan(dateTime);
+        }
+    }
+}
